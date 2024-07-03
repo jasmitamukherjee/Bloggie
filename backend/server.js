099909e108e2f1559ada82785c902e9c6e9a4515
jasmitamukherjee <jasmitamukherjee4@gmail.com>
2024-07-03 14:25:05 +0530

    api for creating post

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,32 @@
+const dotenv = require("dotenv");
+dotenv.config();
 const express=require("express");
+const Post= require("./models/Post/Post");
+const connectDB = require("./utils/connectDB");
+
+connectDB();
 const app= express();
+
 const PORT = 5000;
-app.listen(PORT,console.log(`Server is up and running at :${PORT}`))
+//middlewares
+app.use(express.json())
+
+//create post
+app.post("/api/v1/posts/create",async(req,res)=>{
+    try {
+        //get payload
+        const postData=req.body;
+        const postCreated=await Post.create(postData);
+        res.json({
+            status:"success",
+            message:"Post created Successfully",
+            postCreated,
+        });
+    } catch (error) {
+        console.log(error);
+        res.json(error);
+        
+    }
+})
+
+app.listen(PORT,console.log(`Server is up and running at : ${PORT}`))
