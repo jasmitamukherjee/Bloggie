62341470c050f3a61b64cfe3c96158b2f6ad6f16
jasmitamukherjee <jasmitamukherjee4@gmail.com>
2024-07-07 02:47:51 +0530

    Update usersRouter.js

--- backend/routers/users/usersRouter.js
+++ backend/routers/users/usersRouter.js
@@ -11,8 +11,7 @@ const upload = multer({storage})
 //registration 
 usersRouter.post("/api/v1/users/register",userController.register);
 usersRouter.post("/api/v1/users/login",userController.login);
-usersRouter.get("/api/v1/users/auth/google",userController.googleAuth);
-usersRouter.get("/api/v1/users/auth/google/callback",userController.googleAuthCallback);
+
 usersRouter.get("/api/v1/users/checkAuthenticated",userController.checkAuthenticated);
 usersRouter.post("/api/v1/users/logout",userController.logout);
 usersRouter.get("/api/v1/users/profile",isAuthenticated,userController.profile);
