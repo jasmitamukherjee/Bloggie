0dbf52e59ca97cda6e98d0362f7928efef315efd
jasmitamukherjee <jasmitamukherjee4@gmail.com>
2024-07-07 02:32:24 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -38,7 +38,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json())
 const corsOptions = {
-    origin:process.env.BASE_URL || "http://localhost:5173",
+    origin:process.env.BASE_URL || "https://bloggie-iwa0.onrender.com",
     
     credentials:true
 }
