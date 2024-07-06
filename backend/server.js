41ab902fea2e34c8c0b785ae6a6280e256106c4e
jasmitamukherjee <jasmitamukherjee4@gmail.com>
2024-07-07 00:37:36 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,7 @@ if(today.getMonth() !== tomorrow.getMonth())
 })
 const app= express();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 //middlewares
 
 app.use(express.json())
