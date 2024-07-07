59b700f7a899fdc1bd2f99017597d98c6f0ea467
jasmitamukherjee <jasmitamukherjee4@gmail.com>
2024-07-07 13:20:52 +0530

    Update PublicNavbar.jsx

--- frontend/src/components/Navbar/PublicNavbar.jsx
+++ frontend/src/components/Navbar/PublicNavbar.jsx
@@ -60,7 +60,7 @@ const PublicNavbar = () => {
                     <div className='flex items-center'>
                         <div className='flex-shrink-0'>
                             <Link
-                            to="/create-post"
+                            to="/register"
                             className='relative inline-flex items-center gap-x-1.5 rounded-md bg-orange-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-orange-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 animate-pulse'>
                                 <PlusIcon className='-ml-0.5 h-5 w-5' aria-hidden="true"/>
                                 Create Post
